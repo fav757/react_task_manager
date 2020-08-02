d5a3f3b29c5863e0bdafc0af4c3c3bcaf9995643
fav757 <fav755@gmail.com>
2020-08-02 22:48:07 +0300

    Header components were combined

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import styles from './Header.module.css';
+import BurgerButton from '../BurgerButton/BurgerButton';
+import SearchPanel from '../SearchPanel/SearchPanel';
+import Logo from '../Logo/Logo';
 
 function Header() {
   return (
-    <header>Header</header>
+    <header className={styles.wrap}>
+      <BurgerButton />
+      <Logo />
+      <SearchPanel />
+    </header>
   );
 }
 
