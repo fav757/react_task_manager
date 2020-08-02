afb70eed368e2debd8911c943be004ca8dd33a37
fav757 <fav755@gmail.com>
2020-08-02 22:41:25 +0300

    Formated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
     </div>
   );
