c0b08db5fb709e62da2db0493e8852debff5def0
fav757 <fav755@gmail.com>
2020-08-06 12:36:36 +0300

    burger menu and logo moved to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import styles from './Header.module.css';
-import BurgerButton from '../BurgerButton/BurgerButton';
 import SearchPanel from '../SearchPanel/SearchPanel';
-import Logo from '../Logo/Logo';
 import AuthorCard from '../AuthorCard/AuthorCard';
+import burgerIcon from './burger_icon.svg';
+import Icon from '../Icon/Icon';
+import logoIcon from './logo_placeholder.png';
+
+function BurgerButton() {
+  const handleClick = () => alert('works');
+
+  return (
+    <div onClick={handleClick}>
+      <Icon title='Burger' icon={burgerIcon} />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className={styles.logo}>
+      <img className={styles.icon} src={logoIcon} alt='logo' />
+      <h1 className={styles.header}>Logo</h1>
+    </div>
+  );
+}
 
 function Header() {
   return (
