a79d0c290c04f637d0b47432c4ad3007b6de6d08
fav757 <fav755@gmail.com>
2020-08-07 07:38:03 +0300

    style improvements

--- src/components/Workspace/workspaceReducer.js
+++ src/components/Workspace/workspaceReducer.js
@@ -4,11 +4,35 @@ import notePlaceholder2 from './notePlaceholder2.jpg';
 
 const initialState = [
   {
-    images: [notePlaceholder1, notePlaceholder2],
+    images: [notePlaceholder1],
     title: 'Inital note',
     text: 'This is the your first note provided to test application',
     tags: ['home', 'work'],
     color: 'lightskyblue',
+    system: 'all',
+  },
+  {
+    images: [notePlaceholder2],
+    title: 'Inital note 2',
+    text: 'This is the your first note provided to test application',
+    tags: ['home', 'work'],
+    color: 'lightgreen',
+    system: 'all',
+  },
+  {
+    images: [notePlaceholder1],
+    title: 'Inital note 3',
+    text: 'This is the your first note provided to test application',
+    tags: ['home', 'work'],
+    color: 'lightsalmon',
+    system: 'all',
+  },
+  {
+    images: [notePlaceholder2],
+    title: 'Inital note 4',
+    text: 'This is the your first note provided to test application',
+    tags: ['home', 'work'],
+    color: 'white',
     system: 'all',
   }
 ];
